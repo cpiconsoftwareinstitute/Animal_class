99ea6d43436ff2bd89d9bd6e4947830c083c6626
cpiconsoftwareinstitute <cpicon@softwareinstitute.com>
2022-01-25 12:41:42 +0000

    Increasing coverage 5

--- src/main/java/com/carlos/picon/Mammal.java
+++ src/main/java/com/carlos/picon/Mammal.java
@@ -2,7 +2,7 @@ package com.carlos.picon;
 
 public abstract class Mammal extends Animal{
 
-    public void breath(){
-        System.out.println("The mammal is breathing");
+    public String breath(){
+        return "The mammal is breathing";
     }
 }
--- src/test/java/com/carlos/picon/test/AnimalTest.java
+++ src/test/java/com/carlos/picon/test/AnimalTest.java
@@ -14,17 +14,21 @@ public class AnimalTest {
         Animal testAnimal2 = new Dog();
         Animal testAnimal3 = new Eagle();
         Animal testAnimal4 = new Pigeon();
+        Mammal testAnimal5 = new Human();
         //Human
         testAnimal.set_age(16);
         testAnimal.timePasses();
         testAnimal.timePasses();
         testAnimal.getHunger();
+        testAnimal5.breath();
+        //Dog
         testAnimal2.set_age(16);
         testAnimal3.set_age(16);
         testAnimal4.set_age(16);
 
         assertEquals(testAnimal.get_age(), 16, "Wrong age");
         assertEquals(testAnimal.getHunger(), 2, "Wrong age");
+        assertEquals(testAnimal5.breath(), "The mammal is breathing", "Wrong age");
 
         assertEquals(testAnimal2.get_age(), 16, "Wrong age");
         assertEquals(testAnimal3.get_age(), 16, "Wrong age");
